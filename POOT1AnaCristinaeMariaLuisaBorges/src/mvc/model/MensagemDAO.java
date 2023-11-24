15e70c5fe489ae1709008535edd5b06b9613bb46
Maria Luísa Borges <mb780544@gmail.com>
2023-11-24 17:12:20 -0300

    Arquivo inicial

---
+++ POOT1AnaCristinaeMariaLuisaBorges/src/mvc/model/MensagemDAO.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mvc.model;
+
+/**
+ *
+ * @author Ana Cristina e Maria Luisa
+ */
+public class MensagemDAO {
+
+    Mensagem[] mensagens = new Mensagem[100];
+
+    public MensagemDAO() {
+    }
+    
+    public MensagemDAO(PessoaDAO origem, PessoaDAO destino){
+        Pessoa p1 = origem.buscaPessoaLogin("ana", "teste");
+        Pessoa p2 = origem.buscaPessoaLogin("maria", "teste");
+        
+        Mensagem m1 = new Mensagem();
+        m1.setMensagem("Oi, como vai?");
+        m1.setPessoaOrigem(p1);
+        m1.setPessoaDestino(p2);
+        this.adiciona(m1);
+        
+        Mensagem m2 = new Mensagem();
+        m2.setMensagem("Vou bem e voce?");
+        m2.setPessoaOrigem(p2);
+        m2.setPessoaDestino(p1); 
+        this.adiciona(m2);
+        
+        Mensagem m3 = new Mensagem();
+        m3.setMensagem("Voce viu o video da capybara? Capybara, capybara, capybara~");
+        m3.setPessoaOrigem(p1);
+        m3.setPessoaDestino(p2);
+        this.adiciona(m3);
+        
+        Mensagem m4 = new Mensagem();
+        m4.setMensagem("SIMMM! Uso a musica desse video para fazer meus treinos matinais, fico muito mais empolgada!");
+        m4.setPessoaOrigem(p2);
+        m4.setPessoaDestino(p1);
+        this.adiciona(m4);
+    }
+    
+    public boolean mandarMensagem(Pessoa origem, Pessoa destino, String novaMensagem) {
+        Mensagem mensagem = new Mensagem();
+        boolean check = false;
+        for (Mensagem m : mensagens) {
+            if(m != null){
+                if (m.getPessoaOrigem().equals(origem) && m.getPessoaDestino().equals(destino)) {
+                    mensagem.setMensagem(m.getMensagem());
+                    m.setMensagem(mensagem.getMensagem()+ "\n" + novaMensagem);
+                    check = true;
+                } 
+                else {
+                }
+            }
+        }
+        return check;
+    }
+    
+    public boolean adiciona(Mensagem mensagem) {
+        int proximaPosicaoLivre = this.proximaPosicaoLivre();
+        if (proximaPosicaoLivre != -1) {
+            mensagens[proximaPosicaoLivre] = mensagem;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean ehVazio() {
+        for (Mensagem mensagem : mensagens) {
+            if (mensagem != null) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public void mostrarTodos() {
+        boolean tem = false;
+        for (Mensagem mensagem : mensagens) {
+            if (mensagem != null) {
+                System.out.println(mensagem.getMensagem());
+                tem = true;
+            }
+        }
+        if (!tem) {
+            System.out.println("Não há mensagem cadastrada.");
+        }
+    }
+
+    public String buscaPorMensagem(long id) {
+        for (Mensagem mensagem : mensagens) {
+            if (mensagem != null && mensagem.getId() == id) {
+                return mensagem.getMensagem();
+            }
+        }
+        return null;
+    }
+
+    public boolean remover(String mensagem) {
+        for (int i = 0; i < mensagens.length; i++) {
+            if (mensagens[i] != null && mensagens[i].getMensagem().equals(mensagem)) {
+                mensagens[i] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int proximaPosicaoLivre() {
+        for (int i = 0; i < mensagens.length; i++) {
+            if (mensagens[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
---
+++ POOT1AnaCristinaeMariaLuisaBorges/src/mvc/model/SeguirDAO.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package mvc.model;
+
+/**
+ *
+ * @author mb780
+ */
+public class SeguirDAO {
+
+    Seguir[] seguindo = new Seguir[20];
+
+    public SeguirDAO(PessoaDAO p) {
+        Pessoa p1 = p.buscaPessoaLogin("ana", "teste");
+        Pessoa p2 = p.buscaPessoaLogin("maria", "teste");
+
+        Seguir s1 = new Seguir();
+        s1.setPessoaOrigem(p1);
+        s1.setPessoaDestino(p2);
+        this.adiciona(s1);
+        
+        Seguir s2 = new Seguir();
+        s2.setPessoaOrigem(p2);
+        s2.setPessoaDestino(p1);
+        this.adiciona(s2);
+    }
+
+    public boolean começarASeguir(Pessoa origem, Pessoa destino) {
+        boolean check = false;
+        Seguir seguir = buscarSeguidor(origem, destino);
+        if (seguir == null) {
+            Seguir segues = new Seguir();
+            segues.setPessoaOrigem(origem);
+            segues.setPessoaDestino(destino);
+            this.adiciona(seguir);
+            
+            check = true;
+        } else {
+            check = false;
+        }
+        return check;
+    }
+
+    public boolean deixarDeSeguir(Pessoa origem, Pessoa destino) {
+        boolean check;
+        Seguir seguir = buscarSeguidor(origem, destino);
+        if (seguir != null) {
+            for (Seguir s1 : seguindo) {
+                if (s1 != null && s1.equals(seguir)) {
+                    s1 = null;
+                }
+            }
+            check = true;
+        } else {
+            check = false;
+        }
+        return check;
+    }
+
+    public Seguir buscarSeguidor(Pessoa origem, Pessoa destino) {
+        for (Seguir s : seguindo) {
+            if(s != null){
+                if (s.getPessoaOrigem().equals(origem) && s.getPessoaDestino().equals(destino)) {
+                    System.out.println(s.toString());
+                return s;
+                } 
+            }   
+        }
+        return null;
+    }
+
+    private int proximaPosicaoLivre() {
+        for (int i = 0; i < seguindo.length; i++) {
+            if (seguindo[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+    public boolean adiciona(Seguir seguir) {
+        int proximaPosicaoLivre = this.proximaPosicaoLivre();
+        if (proximaPosicaoLivre != -1) {
+            seguindo[proximaPosicaoLivre] = seguir;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean ehVazio() {
+        for (Seguir seguir : seguindo) {
+            if (seguir != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void mostrarTodos(Pessoa p) {
+            
+        boolean tem = false;
+        for (Seguir seguir : seguindo) {
+            if (seguir != null  && seguir.getPessoaDestino() != p && seguir.getPessoaOrigem().equals(p)) {
+                System.out.println(seguir.toString());
+                tem = true;
+            }
+        }
+        if (!tem) {
+            System.out.println("Nao ha seguidores ainda.");
+        }
+    }
+    
+    
+}
